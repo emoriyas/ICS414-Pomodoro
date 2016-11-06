6a806544da449d712977dae16a0b3c3f1330990d
chocowzzrd <samuel_h_park@hotmail.com>
2016-11-05 19:12:45 -1000

    initial push
    Working timer that can be set with input fields and sumbit buttons.

---
+++ timer.js
@@ -0,0 +1,60 @@
+function startTimer(duration, display, bt) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+		var bool = 0;
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+/*
+        if (--timer < 0) {
+            timer = duration;
+        }
+*/
+		--timer;
+		if (timer < 0 && bool == 0) {
+            timer = bt;
+			bool = 1;
+        } else if (timer < 0 && bool == 1) {
+			timer = duration;
+			bool = 0;
+		}
+		
+		
+    }, 1000);
+}
+ 
+
+window.onload = function () {
+    if (window.location.href.match('runningTimer.html') != null) {
+		sTime = sessionStorage.getItem('t1') * 60;
+		bTime = sessionStorage.getItem('t2') * 60;
+			display = document.querySelector('#time');
+		startTimer(sTime, display, bTime);
+	}
+};
+
+	
+$(document).ready(function(){
+    $("#Start").click(function() {
+        sTime = sessionStorage.getItem('t1');
+		bTime = sessionStorage.getItem('t2');
+		window.location.assign("runningTimer.html");
+    });
+});
+
+$(document).ready(function(){
+    $("#Set").click(function() {
+        sTime = document.getElementById("num1").value;
+		bTime = document.getElementById("num2").value;
+		$("#t1").empty();
+		$("#t2").empty();
+		$("#t1").append(sTime);
+		$("#t2").append(bTime);
+		sessionStorage.setItem('t1', sTime);
+		sessionStorage.setItem('t2', bTime);
+    });
+});
